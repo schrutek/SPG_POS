3c11391c63bf4aa222631d7ac14ca247ca4dead2
Martin Schrutek <m_schenkir@gmx.at>
2020-09-23 12:01:18 +0200

    --

---
+++ CSharp/CSharp Basics/05_Vererbung/Spg.Inheritance.Tests/Spg.Inheritance.Tests/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spg.Inheritance.Tests
+{
+	public abstract class Person
+	{
+		public int Nr { get; }
+		public string Zuname { get; set; }
+		/// <summary>
+		/// Dieses read-only Property ist abstrakt, es muss daher in nachfolgenden Klassen
+		/// implementiert werden, wenn sie nicht ebenfalls abstrakt sein sollen.
+		/// Es muss jedoch ebenfalls als read-only Property implementiert werden.
+		/// </summary>
+		public abstract bool IstVolljaehrig { get; }
+		/// <summary>
+		/// Implementierungen in den abgeleiteten Klassen sollen diese Methode mit override
+		/// überschreiben können. Daher müssen wir sie mit virtual kennzeichnen.
+		/// </summary>
+		/// <returns></returns>
+		public virtual string GetAccountname()
+		{
+			// Substring liefert eine Exception, wenn Zuname kürer als 3 Stellen ist.
+			int len = Math.Min(Zuname?.Length ?? 0, 3);
+			return $"{Zuname?.Substring(0, len)?.ToLower() ?? ""}{Nr:0000}";
+		}
+		public Person(int nr)
+		{
+			Nr = nr;
+		}
+		public string HiddenMethod()
+		{
+			return "HiddenMethod in Person";
+		}
+		/// <summary>
+		/// Mit base greifen wir auf die Implementierung von object zu.
+		/// </summary>
+		/// <returns></returns>
+		public override string ToString()
+		{
+			string objectToString = base.ToString();
+			return $"base.ToString() von Person {Nr} ist {objectToString}";
+		}
+	}
+}
--- CSharp/CSharp Basics/06_DependencyInjection/Readme.txt
+++
@@ -1,5 +0,0 @@
-Ein Beispiel ist in "C:\HTL\Git_Vorebereitung\Pos3xHif\01_Types\05_Equality\SPG.3BHIF.Equals" hinein verwurschtet.
-
-Muss man rausextrahieren.
-
-Die Übung aus Michi-GIT ist noch zu implementieren
