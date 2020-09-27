ec3851b5adc0e199c3246b5a5d0d9319d9a8411f
Martin Schrutek <m_schenkir@gmx.at>
2020-09-27 20:27:37 +0200

    --

---
+++ CSharp/MVC/02_Grundlagen/Spg.MvcTest/src/Spg.MvcTest.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Spg.MvcTest.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CSharp/MVC/02_Grundlagen/Spg.MvcTest/src/Spg.MvcTest.WebApp/Models/Schoolclass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spg.MvcTest.WebApp.Models
+{
+    public class Schoolclass
+    {
+        public string Name { get; set; }
+
+        public string Abteilung { get; set; }
+    }
+}
