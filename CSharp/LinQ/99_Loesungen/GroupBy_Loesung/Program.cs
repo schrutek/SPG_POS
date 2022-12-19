5777275bf7e40bb41a51bf344470f2d5310a0884
schrutek <martin@schrutek>
2022-12-19 14:22:01 +0100

    Commit

---
+++ CSharp/LinQ/99_Loesungen/GroupBy_Loesung/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Grouping.Model;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Grouping
+{
+    class Pupil
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            // *************************************************************************************
+            // Schreibe in den nachfolgenden Übungen statt der Zeile
+            // object resultX = null;
+            // die korrekte LINQ Abfrage. Verwende den entsprechenden Datentyp statt object.
+            // Du kannst eine "schöne" (also eingerückte) Ausgabe der JSON Daten erreichen, indem
+            // du die Variable WriteIndented auf true setzt.
+            // *************************************************************************************
+            var WriteIndented = false;
+            var serializerOptions = new JsonSerializerOptions { WriteIndented = WriteIndented };
+            TestsData db = await TestsData.FromFile("db/tests.json");
+
+
+            // *************************************************************************************
+            // ÜBUNG 1: Erstelle für jeden Lehrer eine Liste der Fächer, die er unterrichtet. Es
+            // sind nur die ersten 10 Datensätze auszugeben. Das kann mit
+            // .OrderBy(t=>t.TeacherId).Take(10)
+            // am Ende der LINQ Anweisung gemacht werden. Hinweis: Verwende Distinct für die
+            // liste der Unterrichtsgegenstände.
+            // *************************************************************************************
+
+            var result1 = db.Lesson
+                .GroupBy(l => l.L_Teacher)
+                .Select(g => new
+                {
+                    TeacherId = g.Key,
+                    Subjects = g.Select(g2 => new
+                    {
+                        g2.L_Subject
+                    }).Distinct()
+                })
+                .OrderBy(t => t.TeacherId).Take(10);
+
+            Console.WriteLine("RESULT1");
+            Console.WriteLine(JsonSerializer.Serialize(result1, serializerOptions));
+
+            // *************************************************************************************
+            // ÜBUNG 2: Die 5AHIF möchte wissen, in welchem Monat sie welche Tests hat.
+            //          Hinweis: Mit den Properties Month und Year kann auf das Monat bzw. Jahr
+            //          eines DateTime Wertes zugegriffen werden. Die Ausgabe in DisplayMonth kann
+            //          $"{mydate.Year:00}-{mydate.Month:00}" (mydate ist zu ersetzen)
+            //          erzeugt werden
+            // *************************************************************************************
+
+            var result2 = db.Test
+                .Where(t => t.TE_Class == "5AHIF")
+                .GroupBy(t => $"{t.TE_Date.Year:00}-{t.TE_Date.Month:00}")
+                .Select(g => new
+                {
+                    DisplayMonth = g.Key,
+                    Tests = g.Select(g2 => new
+                    {
+                        Date = g2.TE_Date,
+                        Teacher = g2.TE_Teacher,
+                        Subject = g2.TE_Subject,
+                    })
+                });
+
+            Console.WriteLine("RESULT2");
+            Console.WriteLine(JsonSerializer.Serialize(result2, serializerOptions));
+
+
+            // *************************************************************************************
+            // ÜBUNG 3: Jeder Schüler der 5AHIF soll eine Übersicht bekommen, welche Tests er pro Fach
+            //          abgeschlossen hat.
+            //          Es sind nur die ersten 2 Schüler mit OrderBy(p => p.P_ID).Take(2) am Ende des
+            //          Statements auszugeben.
+            //          Hinweis: Beachte die Datenstruktur in der Ausgabe.
+            //   Pupil                           <-- Zuerst wird der Schüler projiziert (Select)
+            //     |
+            //     +-- P_ID
+            //         P_Firstname
+            //         P_Lastname
+            //         Tests                     <-- Hier soll nach Subject gruppiert werden
+            //           |
+            //           +---- Subject           <-- y der GrupKepierung
+            //           +---- Termine           <-- Projektion der Gruppierung
+            //                    |    
+            //                    +------ TE_Teacher
+            //                    +------ TE_Date
+            //                    +------ TE_Lesson
+            // *************************************************************************************
+
+            var result3 = db.Pupil
+                .Where(p => p.P_Class == "5AHIF")
+                .Select(p => new
+                {
+                    p.P_ID,
+                    p.P_Firstname,
+                    p.P_Lastname,
+                    Tests = p.P_ClassNavigation.Tests
+                    .GroupBy(p => p.TE_Subject)
+                    .Select(g => new
+                    {
+                        Subject = g.Key,
+                        Termine = g.Select(g2 => new
+                        {
+                            g2.TE_Teacher,
+                            g2.TE_Date,
+                            g2.TE_Lesson
+                        })
+                    })
+                })
+                .OrderBy(p => p.P_ID).Take(2);
+
+            Console.WriteLine("RESULT3");
+            Console.WriteLine(JsonSerializer.Serialize(result3, serializerOptions));
+
+            // *************************************************************************************
+            // ÜBUNG 4: Wie viele Klassen sind pro Tag und Stunde gleichzeitig im Haus?
+            //          Hinweis: Gruppiere zuerst nach Tag und Stunde in Lesson. Für die Ermittlung
+            //          der Klassenanzahl zähle die eindeutigen KlassenIDs, indem mit Distinct eine
+            //          Liste dieser IDs (C_ID) erzeugt wird und dann mit Count() gezählt wird.
+            //          Es sind mit OrderByDescending(g=>g.ClassCount).Take(5) nur die 5
+            //          "stärksten" Stunden auszugeben.
+            // *************************************************************************************
+
+            var result4 = db.Lesson
+                .GroupBy(l => new { l.L_Day, l.L_Hour })
+                .Select(g => new
+                {
+                    Day = g.Key.L_Day,
+                    Hour = g.Key.L_Hour,
+                    ClassCount = g.Select(g2 => g2.L_Class).Distinct().Count()
+                })
+                .OrderByDescending(g => g.ClassCount).Take(5);
+
+            Console.WriteLine("RESULT4");
+            Console.WriteLine(JsonSerializer.Serialize(result4, serializerOptions));
+
+            // *************************************************************************************
+            // ÜBUNG 5: Wie viele Klassen gibt es pro Abteilung?
+            // *************************************************************************************
+
+            var result5 = db.Schoolclass
+                .GroupBy(s => s.C_Department)
+                .Select(g => new
+                {
+                    Department = g.Key,
+                    KlassenCount = g.Count()
+                });
+
+            Console.WriteLine("RESULT5");
+            Console.WriteLine(JsonSerializer.Serialize(result5, serializerOptions));
+
+            // *************************************************************************************
+            // ÜBUNG 6: Wie die vorige Übung, allerdings sind nur Abteilungen
+            //          mit mehr als 10 Klassen auszugeben.
+            //          Hinweis: Filtere mit Where nach dem Erstellen der Objekte mit Department
+            //                   und Count
+            // *************************************************************************************
+
+            var result6 = db.Schoolclass
+                .GroupBy(s => s.C_Department)
+                .Select(g => new
+                {
+                    Department = g.Key,
+                    KlassenCount = g.Count()
+                })
+                .Where(s => s.KlassenCount > 10);
+
+            Console.WriteLine("RESULT6");
+            Console.WriteLine(JsonSerializer.Serialize(result6, serializerOptions));
+
+            // *************************************************************************************
+            // ÜBUNG 7: Wann ist der letzte Test (Max von TE_Date) pro Lehrer und Fach der 5AHIF
+            //          in der Tabelle Test?
+            // *************************************************************************************
+
+            object result7 = db.Test
+                .Where(t => t.TE_Class == "5AHIF")
+                .GroupBy(t => new { t.TE_Teacher, t.TE_Subject })
+                .Select(g => new
+                {
+                    g.Key.TE_Teacher,
+                    g.Key.TE_Subject,
+                    LastTest = g.Max(g2 => g2.TE_Date)
+                });
+
+            Console.WriteLine("RESULT7");
+            Console.WriteLine(JsonSerializer.Serialize(result7, serializerOptions));
+        }
+    }
+}
---
+++ CSharp/LinQ/99_Loesungen/Projektion_Loesung/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LinqUebung2.Model;
+using Newtonsoft.Json;
+
+namespace LinqUebung2
+{
+    class Program
+    {
+        static void WriteJson(object data, string title = "", bool indent = false)
+        {
+            string result = JsonConvert.SerializeObject(data, indent ? Formatting.Indented : Formatting.None);
+            ConsoleColor oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(title);
+            Console.ForegroundColor = oldColor;
+            Console.WriteLine(result);
+        }
+
+        static void Main(string[] args)
+        {
+            PruefDb db = PruefDb.FromMockup();
+
+            // *************************************************************************************
+            // MUSTERBEISPIELE
+            // *************************************************************************************
+
+            // Liefere eine Liste aller Prüfungsfächer (IEnumerable<string>)
+            // Mit Select kann man sich aussuchen, welche Properties geliefert werden.
+            // Select nennt man auch "Projektion"
+
+            IEnumerable<string> uebung1 = db.Pruefungen.Select(p => p.Fach);
+            // Liste beinhaltet D, E, E, AM, D, AM, ...
+
+            IEnumerable<string> uebung2 = db.Pruefungen.Select(p => p.Fach).Distinct();
+            // Liste beinhaltet D, E, AM, POS, DBI (jedes Fach nur 1x)
+
+            // Liefere eine Liste aller Schüler mit der Anzahl der Prüfungen
+            // als Objekt Name, Vorname, Anzahl
+            // Der Compiler legt eine anonyme Klasse an:
+            // class A {
+            //   string Name {get;set;}
+            //   string Vorname {get;set;}
+            //   int Anzahl {get;set;}
+            // }
+            var uebung3 = db.Schuelers.Select(s => new
+            {
+                Name = s.Name,
+                Vorname = s.Vorname,
+                Anzahl = s.Pruefungen.Count()   // Propertynamen festlegen
+            }).OrderBy(x => x.Anzahl).ThenBy(x => x.Name);
+
+            // Welche Schüler haben mehr als 6 Prüfungen?
+            var uebung4 = uebung3.Where(p => p.Anzahl > 6);
+
+            // Liefere ein JSON Objekt mit folgendem Aufbau:
+            // {
+            //    Name: Mustermann,
+            //    Vorname: Max,
+            //    Pruefer: [KY, FAV]
+            // },...
+            var uebung5 = db.Schuelers.Select(s => new
+            {
+                s.Name,
+                s.Vorname,
+                Pruefer = s.Pruefungen.Select(p => p.Pruefer).Distinct()
+            });
+            WriteJson(uebung5, "Beispiel 5 - Schüler mit Prüfer", true);
+
+
+            // Liefere ein JSON Objekt mit folgendem Aufbau:
+            // {
+            //    Name: "Mustermann,"
+            //    Vorname: "Max",
+            //    db.Pruefungen: [{"Pruefer"="KY", "Fach"="AM"}, ...]
+            // },...
+            var uebung6 = db.Schuelers
+                .Where(s => s.Id < 1010)
+                .Select(s => new
+                {
+                    s.Name,
+                    s.Vorname,
+                    Pruefungen = s.Pruefungen.Select(p => new
+                    {
+                        p.Pruefer,
+                        p.Fach
+                    }),
+                    Anzahl = s.Pruefungen.Count(),
+                });
+            WriteJson(uebung6, "Beispiel 6 - Schüler mit Prüfungen", true);
+
+
+            // *************************************************************************************
+            // ÜBUNGEN
+            // Schreibe in den nachfolgenden Übungen statt der Zeile
+            // object resultX = null;
+            // die korrekte LINQ Abfrage. Verwende den entsprechenden Datentyp statt object.
+            // *************************************************************************************
+
+            // 1. Drucke eine Liste aller Fächer. Hinweis: Verwende Distinct, um doppelte Einträge
+            //    zu entfernen. Speichere das Ergebnis in IEnumerable<string>.
+
+            IEnumerable<string> result1 = db.Pruefungen
+                .Select(p => p.Fach)
+                .Distinct();
+            
+            Console.WriteLine("Die Prüfungsfächer sind " + String.Join(",", result1));
+
+            // 2. Ermittle die schlechteste Prüfungsnote in E. Dabei verwende allerdings die Max
+            //    Funktion ohne Parameter, indem vorher eine Liste von int Werten erzeugt wird.
+
+            int result2 = db.Pruefungen
+                .Where(p => p.Fach == "E")
+                .Select(p => p.Note)
+                .Max();
+            
+            Console.WriteLine($"Die schlechteste E Note ist {result2}");
+
+            // 3. Liefere eine Liste mit Klasse, Name, Vorname und der Anzahl der Prüfungen
+            //    (soll Anzahl heißen) der 3CHIF. Sortiere die Liste nach Klasse und Name.
+            //    Hinweis: Verwende OrderBy und dann ThenBy.
+
+            var result3 = db.Schuelers
+                .Where(s => s.Klasse == "3CHIF")
+                .OrderBy(s => s.Klasse)
+                .ThenBy(s => s.Name)
+                .Select(s => new
+                {
+                    s.Klasse,
+                    s.Vorname,
+                    s.Name,
+                    Anzahl = s.Pruefungen.Count(),
+                });
+            
+            Console.WriteLine("Beispiel 3");
+            result3.ToList().ForEach(s => { Console.WriteLine($"   {s.Klasse}: {s.Name} {s.Vorname} hat {s.Anzahl} Prüfungen."); });
+
+            // 4. Liefere eine Liste der Schüler (Klasse, Name, Vorname) der 3BHIF mit ihren Prüfungsfächern. 
+            //    Die Fächer sollen als Stringliste (IEnumerable<string> ohne doppelte Einträge 
+            //    in das Property Faecher gespeichert werden.
+
+            var result4 = db.Schuelers
+                .Where(s => s.Klasse == "3BHIF")
+                .Select(s => new
+                {
+                    s.Klasse,
+                    s.Name,
+                    s.Vorname,
+                    Faecher = s.Pruefungen.Select(p => p.Fach).Distinct()
+                });
+            
+            Console.WriteLine("Beispiel 4");
+            result4.ToList().ForEach(s => { Console.WriteLine($"   {s.Klasse}: {s.Name} {s.Vorname} hat {String.Join(",", s.Faecher)}"); });
+
+            // 5. Liefere eine Liste aller Schüler der 3AHIF (Name, Vorname) mit ihren negativen 
+            //    Prüfungen. Dabei soll unter dem Property "NegativePruefungen"
+            //    ein neues Objekt mit Datum und Fach aller negativen Prüfungen (Note = 5) 
+            //    erzeugt werden. Es sind natürlich 2 new Anweisungen nötig.
+            //    Außerdem sollen nur Schüler der 3AHIF berücksichtigt werden.
+
+            var result5 = db.Schuelers
+                .Where(s => s.Klasse == "3AHIF")
+                .Select(s => new
+                {
+                    s.Name,
+                    s.Vorname,
+                    NegativePruefungen = s.Pruefungen
+                        .Where(p => p.Note == 5)
+                        .Select(p => new
+                        {
+                            p.Datum,
+                            p.Fach,
+                            p.Note
+                        })
+                });
+            
+            Console.WriteLine("Beispiel 5");
+            foreach (var schueler in result5)
+            {
+                Console.WriteLine($"   Negative Prüfungen von {schueler.Name} {schueler.Vorname}:");
+                foreach (var pruef in schueler.NegativePruefungen)
+                {
+                    Console.WriteLine($"      {pruef.Fach} am {pruef.Datum.ToString("dd.MM.yyyy")}");
+                }
+            }
+
+            // 6. Liefere eine Liste aller Prüfer mit ihren besten und schlechtesten 
+            //    Prüfungsergebnis. Gehe dabei so vor: Erzeuge mit Select und Distinct
+            //    eine Stringliste aller Prüfer. Davon ausgehend erzeuge - wieder mit Select - 
+            //    ein neues Objekt mit den Properties Pruefer, BesteNote und SchlechtesteNote.
+            //    BesteNote und SchlechtesteNote werden wieder von allen Prüfungen mit entsprechdem
+            //    Where Filter berechnet.
+            //    Später werden wir dieses Problem mit Group by lösen.
+
+            var pruefers = db.Pruefungen.Select(p => p.Pruefer).Distinct();
+            var result6 = pruefers
+                .Select(p => new
+                {
+                    Pruefer = p,
+                    BesteNote = db.Pruefungen.Where(pr => pr.Pruefer == p).Min(pr => pr.Note),
+                    SchlechtesteNote = db.Pruefungen.Where(pr => pr.Pruefer == p).Max(pr => pr.Note),
+                });
+            
+            Console.WriteLine("Beispiel 6");
+            result6.ToList().ForEach(p => { Console.WriteLine($"   {p.Pruefer} gibt Noten von {p.BesteNote} bis {p.SchlechtesteNote}"); });
+        }
+    }
+}
+
